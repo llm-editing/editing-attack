1a1ce6d862d7ec9748d3ed985bc0e544ec586e61
Baixiang Huang <bhuang15@hawk.iit.edu>
2024-11-08 21:41:40 -0600

    update

--- code/misinfomation_injection.sh
+++ code/misinfomation_injection.sh
@@ -15,7 +15,7 @@ python3 inject_misinfomation.py --editing_method=ICL --hparams_dir=./hparams/ICL
 python3 inject_misinfomation.py --editing_method=ICL --hparams_dir=./hparams/ICL/alpaca-7b
 python3 inject_misinfomation.py --editing_method=ICL --hparams_dir=./hparams/ICL/vicuna-7b
 python3 inject_misinfomation.py --editing_method=ICL --hparams_dir=./hparams/ICL/mistral-7b
-python3 inject_misinfomation.py --editing_method=ICL --hparams_dir=./hparams/ICL/mistral-7b-v2
+python3 inject_misinfomation.py --editing_method=ICL --hparams_dir=./hparastatsms/ICL/mistral-7b-v2
 
 # Long-tail
 python3 inject_misinfomation.py --editing_method=ROME --hparams_dir=./hparams/ROME/llama3-8b --long_tail_data=True --metrics_save_dir=../results/results_long_tail_misinfomation_injection
